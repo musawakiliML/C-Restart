cd23465a51cc4865a0cacd0c16bcc39c8e0a9890
musawakiliML <musawakiliml@hotmail.com>
2022-09-21 12:30:42 +0100

    First Commit exercise

---
+++ C-Hard-Way/ex1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/* This is a Comment */
+int main(int argc, char *argv[]) {
+
+    int distance = 100;
+
+    // this is also a Comment
+    printf("You are %d miles away. \n", distance);
+    return 0;
+}

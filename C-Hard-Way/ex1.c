3699aed3cab02f88741a30fb65fc8b2e80658cf9
musawakiliML <musawakiliml@hotmail.com>
2022-09-21 12:59:17 +0100

    Created a Makefile

--- C-Hard-Way/ex1.c
+++ C-Hard-Way/ex1.c
@@ -4,8 +4,11 @@
 int main(int argc, char *argv[]) {
 
     int distance = 100;
+    int Kilometers = 50;
 
     // this is also a Comment
     printf("You are %d miles away. \n", distance);
+    printf("You Should move at %d Kilometers per hour.\n", Kilometers);
     return 0;
+
 }
